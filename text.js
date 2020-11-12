9e4706a7e73a6ae60ce98f62544fa00006af20fe
Franz-Kim <71546458+Franz-Kim@users.noreply.github.com>
2020-11-12 12:17:56 +0900

    Update text.js

--- text.js
+++ text.js
@@ -20,8 +20,8 @@ export class Text {
         let fontSize = 400;
         //mobile check
         if (mql.matches) { //mobile
-            fontWidth = 80;
-            fontSize = 70;
+            fontWidth =200;
+            fontSize = 100;
         } else { //desktop
             fontWidth = 500;
             fontSize = 400;
