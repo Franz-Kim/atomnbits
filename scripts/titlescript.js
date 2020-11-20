0e73e043723cba7e32b81839503955f4de6a230b
Franz-Kim <71546458+Franz-Kim@users.noreply.github.com>
2020-11-20 18:13:58 +0900

    timeout

--- scripts/titlescript.js
+++ scripts/titlescript.js
@@ -35,3 +35,33 @@ const mouseClickAnimation =()=>{
 }
 
 startbuttone.addEventListener('click', mouseClickAnimation)
+
+
+
+setTimeout(function () {
+    anime({
+        // targets: '.square',
+        targets: everthing,
+        // translateY : -document.documentElement.clientHeight,
+        translateY: [0, -window.innerHeight],
+        easing: 'easeInQuint',
+        loop: false,
+        duration: 1000
+    });
+
+    setTimeout(function () {
+        var mql = window.matchMedia("screen and (max-width: 768px)");
+        if (mql.matches) {
+            anime({
+                targets: everthing,
+                // translateY : -document.documentElement.clientHeight,
+                translateY: [-window.innerHeight, 0],
+                easing: 'easeInQuint',
+                loop: false,
+                duration: 1000
+            });
+        }
+        window.location.href = "main.html";
+    }, 1100);
+
+}, 5000);
